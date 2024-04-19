929551988144b88d929062fa3362b8de8301a25f
BiancaMesa <bianca.koraa@gmail.com>
2024-04-19 12:24:45 +0200

    endpoint to modify an existing song and some part of the readme

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 
+**See all the song**
+Front-end must: 
+- Use the method get
+- Use the route: /songs
+
+**See one song**
+Front-end must: 
+- Use the method get
+- Use the route: /songs/:id
+- Send as url params the id of the song you want to see.
+
+**See all the albums**
+Front-end must: 
+- Use the method get
+- Use the route: /albums
+
 **Add a new song**
-Front end must send in the post the information relating to the song, music_video, writers and albumId, and in that order.
+Front-end must: 
+- Use the method post
+- Use the route: /songs
+- Send as body params the information relating to the song, music_video, writers and album_id, and in that order.
+
+**Modify an existing song**
+Front-end must: 
+- Use the method put
+- Use the route: /songs/:id
+- Front-end must send as url params the id of the song and in the method put the information relating to the song, music_video, writers and album_id, and in that order.
--- src/index.js
+++ src/index.js
@@ -114,30 +114,30 @@ server.get("/songs/:id", async (req, res) => {
 server.post("/songs", async(req, res) => {
     //recogemos los datos que nos ha enviado front con la canción nueva
     //destructuring: creamos un objeto nuevo que tenga toda la información que ha enviado front excepto por el nombre del album y una constante albumId que es el id del album al que pertenece que nos lo ha enviado front (la programadora, no la usuaria)
-    const {albumId, ...newSongTableData} = req.body; 
-    console.log('Info que recibimos de front:', req.body);
-    console.log('Info que meteremos en tabla song:', newSongTableData);
-    console.log('Album Id, clave foránea:', albumId); //number 
-
+    //const {albumId, ...newSongTableData} = req.body; 
     //destructuring para que quede más limpia la query
-    const {song, music_video, writers} = req.body;
+    const {song, music_video, writers, album_id} = req.body;
+    console.log('Info que recibimos de front:', req.body);
+    console.log('Info que meteremos en tabla song:', song, music_video, writers);
+    console.log('Album Id, clave foránea:', album_id); //number 
+
 
     //nos conectamos a la base datos
     const connection = await getDBConnection();
 
-    if(!albumId) {
+    if(!album_id) {
         res.status(400).json({
             status: "error", 
             error: "Please introduce the album of the song"
         });
     } else {
         //hacemos la query a la BD, le envíamos los datos de la canción nueva
-        const newSongQuerySQL = "INSERT INTO song (songName, musicVideo, writtenBy, fk_albumId) VALUES (?, ?, ?, ?)"
+        const newSongQuerySQL = "INSERT INTO song (songName, musicVideo, writtenBy, fk_albumId) VALUES (?, ?, ?, ?)";
         const [newSongResult] = await connection.query(newSongQuerySQL, [
             song, 
             music_video,
             writers, 
-            albumId, //será la clave foránea
+            album_id, //será la clave foránea
         ]);
         console.log(newSongResult);
 
@@ -146,5 +146,53 @@ server.post("/songs", async(req, res) => {
             success: true, 
             id: newSongResult.insertId, 
         })
+    }
+});
+
+
+//Endpoint para modificar una canción 
+server.put("/songs/:id", async (req, res) => {
+    //recogemos el id que nos envía front por url params
+    const song_id = req.params.id; 
+
+    //recogemos los datos que nos envía front por body params
+    const modifySongData = req.body; 
+    const {song, music_video, writers, album_id} = modifySongData;
+    console.log('El album_id es:', album_id);
+    
+
+    //abrimos la conexión con la base de datos
+    const connection = await getDBConnection();
+
+    //controlar errores
+    if(!song_id) {
+        res.status(400).json({
+            status: "error", 
+            error: "No song matches that id"
+        }); 
+    } else {
+        if (!album_id) {
+            res.status(400).json({
+                status: "error", 
+                error: "Please introduce the album of the song"
+            });
+        } else {
+            //hacemos la query a la base datos; actualizamos la canción 
+            const modifySongQuerySQL = "UPDATE song SET songName = ?, musicVideo = ?, writtenBy = ?, fk_albumId = ? WHERE songId = ?";
+            const [modifySongResult] = await connection.query(modifySongQuerySQL, [
+                song, 
+                music_video, 
+                writers, 
+                album_id,
+                song_id
+            ]); 
+
+            console.log(modifySongResult);
+
+            //enviamos la respuesta a front
+            res.status(200).json({
+                success: true, 
+            });
+        }
     }
 })
